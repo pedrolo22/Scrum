1ef95816270670303f154a0f4e2bef62ad562815
Bryan Alexis Freire Viteri <Bryan Alexis Freire Viteri>
2016-05-10 16:33:49 +0200

    Servidor web REST con acceso a la base de datos

--- server.js
+++ server.js
@@ -5,30 +5,59 @@ var express = require('express'),
 var connection = mysql.createConnection({
    host:       'localhost',
    user:       'root',
-   password:   '',
-   database:   'database',
-   port:       1234
+   password:   'admin',
+   database:   'scrum',
+   port:       3306
 });
 
-
-function consulta(request,callback){
-   var result,error;
-   connection.connect(function(error2){
-      if(error2) error = error2;
+connection.connect(function(con_err){
+   console.log(con_err);
+});
+                   
+function query(request,callback){
+   connection.query(request,function(que_err,que_res,que_fileds){
+            if(que_err) callback(undefined,que_err)
+            else callback(que_res,undefined);
    });
-   if(!error){
-      connection.query(request,function(error2,result2){
-         if(error2) error = error2;
-         else result = result2;
-      });
-      connection.end();
-   }
-   callback(result,error);
 }
+
 
 app.use(express.static('public'));
 
-app.get();
-app.post();
+app.get("/get",function(req,res){
+   query('select * from team_member',function(result,err){
+      if(err)console.log(err)
+      res.json(result);
+      res.end();
+   });
+});
+var i = 15;
+app.get("/post",function(req,res){
+   var quer = {
+     id_tm:i+1,
+      Nombre:'Bryan',
+      Rol:'ScrumMaster',
+      Nick:'Bryan',
+      password:'12345',
+      "e-mail":'bryanfv95@gmail.com'
+   };
+   var sql = 'insert into team_member set' + mysql.escape(quer);
+   query(sql,function(result,err){
+      if(err)console.log(err)
+      i++;
+      res.redirect("/get");
+      res.end();
+   })
+});
+
+app.get("/delete",function(req,res){
+   var sql = 'delete from team_member where id_tm = ' + i;
+   query(sql,function(result,err){
+      if(err)console.log(err)
+      i--;
+      res.redirect("/get");
+      res.end();
+   })
+});
 
 app.listen(8080);
